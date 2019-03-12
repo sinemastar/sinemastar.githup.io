25c63459abf89284d24de6baa44bfb70b210f3b6
sinemastar <48469750+sinemastar@users.noreply.github.com>
2019-03-12 00:41:33 -0700

    Fast Website

---
+++ bottom.php
@@ -0,0 +1,70 @@
+<?php global $SMTheme; ?>
+  
+</div></div>
+
+<?php	
+if ($SMTheme->get( 'social', 'showsocial')) {
+	$SMTheme->block_social();
+}
+?>
+<div id='content-bottom' class='container'></div>
+<div id='footer'>
+		<div class='container clearfix'>
+			
+			<?php if ($SMTheme->get("layout","footerwidgets")) { ?>
+			<div class='footer-widgets-container'><div class='footer-widgets'>
+				<div class='widgetf'>
+					<?php 
+					if ( !function_exists("dynamic_sidebar") || !dynamic_sidebar("footer_1") ) {
+						;
+					} ?>
+				</div>
+				
+				<div class='widgetf'>
+					<?php 
+					if ( !function_exists("dynamic_sidebar") || !dynamic_sidebar("footer_2") ) {
+						;
+					} ?>
+				</div>
+				
+				<div class='widgetf widgetf_last'>
+					<?php if ( !function_exists("dynamic_sidebar") || !dynamic_sidebar("footer_3") ) {
+						;
+					} ?>
+				</div>
+			</div></div>
+			<?php } ?>
+			
+		
+		
+		<div class='footer_txt'>
+			<div class='container'>
+				<div class='top_text'>
+				<?php
+                    if ($SMTheme->get( "layout","footertext" )) {
+                        echo $SMTheme->get( "layout","footertext" );
+                    } else { 
+                        ?>Copyright &copy; <?php echo date("Y"); ?>  <a href="<?php echo home_url(); ?>"><?php bloginfo("name"); ?></a><?php
+						echo (get_bloginfo('description'))?' - '.get_bloginfo('description'):'';
+                    }
+                ?>
+				</div>
+				<?php /* 
+					All links in the tag <div class='smthemes'> are attribution of the theme developers and should remain intact. 
+					It's protected by Creative Commons License (http://creativecommons.org/licenses/by/3.0/).
+					Warning! Your site will not be able to work if these links are edited or deleted.
+					You can buy this theme without footer links online at http://smthemes.com/buy/moviestar/
+				*/ ?>
+				<div class='smthemes'>Designed by <a href='http://premiumfreewordpressthemes.com/category/cinema/' target='_blank'>Free Wordpress Movie Themes</a>, thanks to: <a href='http://favorwish.com/' target='_blank'>Nintendo Wii U</a>, <a href='http://smnnews.com' target='_blank'>Sins Metal News</a> and <a href='http://topshowsontv.com/' target='_blank'>Top Shows on TV</a></div>
+			</div>
+			</div>
+		<?php wp_footer(); ?>
+
+		</div> <?php //footer ?>
+		</div>
+</div> <?php //all ?>
+<?php
+	echo $SMTheme->get( "integration","footercode" );
+?>
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Theme Author: http://smthemes.com
+Theme Homepage: http://smthemes.com/moviestar/
+Buy theme: http://smthemes.com/buy/moviestar/
+Support Forums: http://smthemes.com/support/forum/moviestar-free-wordpress-theme/
